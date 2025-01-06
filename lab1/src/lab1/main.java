eda2abefee084b753b53e1f5542bb54ce46bc732
Faaiz Khan-072 <155071219+FaaizKhan-003@users.noreply.github.com>
2025-01-07 03:52:01 +0500

    Add files via upload

---
+++ lab1/src/lab1/main.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package lab1;
+
+/**
+ * 
+ */
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.print("hello Faaiz");
+	}
+
+}
